c22aaaefca60742b563f3626f76c57550ef163b1
Archis Kulkarni <archiskulkarni002@gmail.com>
2025-03-19 16:29:10 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -76,6 +76,7 @@ def scientificMenu():
 
 def main():
     while True:
+        print("\nHello there!")
         print("\nCalculator Menu")
         print("1. Basic Operations")
         print("2. Scientific Operations")
